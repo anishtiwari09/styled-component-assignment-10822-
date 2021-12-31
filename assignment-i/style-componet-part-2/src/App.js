7590b8259719d39e0b1073ec884b1aa4534b44e6
anishtiwari09 <aneeshtiwari24@gmail.com>
2022-01-01 01:41:48 +0530

    navbar-styled

---
+++ assignment-i/style-componet-part-2/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Component/Navbar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Style Component</h1>
+      <Navbar />
+    </div>
+  );
+}
